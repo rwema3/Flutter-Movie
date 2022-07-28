06c334ba0e450433fc1dd5d5db835c5ddd12033f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-28 18:33:05 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -2,28 +2,6 @@ import 'dart:io';
 
 import 'package:common_utils/common_utils.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
-import 'package:flutter/material.dart' hide Action;
-
-import 'package:fish_redux/fish_redux.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:movie/routes/routes.dart';
-import 'actions/app_config.dart';
-import 'actions/api/tmdb_api.dart';
-import 'actions/timeline.dart';
-import 'actions/user_info_operate.dart';
-import 'generated/i18n.dart';
-import 'package:permission_handler/permission_handler.dart';
-
-class App extends StatefulWidget {
-  App({Key key}) : super(key: key);
-
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  final i18n = I18n.delegate;
       home: routes.buildPage('startpage', null),
       onGenerateRoute: (RouteSettings settings) {
         return MaterialPageRoute<Object>(builder: (BuildContext context) {
